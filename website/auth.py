725a912378463b557bcdfd5bec258fbd2947e679
Sunil Raj <sunilsunderrajdev@gmail.com>
2025-01-04 19:47:38 -0500

    Initial checkin

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, jsonify
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<h1>Hello World SSR Login</h1>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<h1>Hello World SSR Logout</h1>"
 
-@auth.route('/signup')
+@auth.route('/sign-up')
 def signup():
-    return "<h1>Hello World SSR Signup</h1>"
+    return render_template("sign-up.html")
--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+
 def home():
     return render_template("home.html")
